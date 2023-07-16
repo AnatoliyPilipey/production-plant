79c2a6b93b478b3580fc57f36d5e2104c4b44208
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-07-16 15:22:12 +0300

    In production\urls.py forwarded to new hall\urls.py and in hall\views.py create views index

--- hall/views.py
+++ hall/views.py
@@ -1,3 +1,22 @@
+from django.db.models import Sum
 from django.shortcuts import render
+from .models import Shift, Foreman
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_shifts = Shift.objects.count()
+    num_foreman = Foreman.objects.count()
+    sum_products = Shift.objects.all().aggregate(Sum("products_produced"))
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_shifts": num_shifts,
+        "num_foreman": num_foreman,
+        "sum_products": sum_products,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "taxi/index.html", context=context)
