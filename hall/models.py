7d0503b9edf1785c3ec8c8387deac563861024d1
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-07-16 08:32:07 +0300

    Create model Foreman(User)

--- hall/models.py
+++ hall/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Foreman(AbstractUser):
+    hired_date = models.DateField()
+    salary = models.DecimalField()
+    description = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        ordering = ["first_name"]
+
+    def __str__(self):
+        return f"{self.first_name} ({self.last_name}, {self.hired_date})"
